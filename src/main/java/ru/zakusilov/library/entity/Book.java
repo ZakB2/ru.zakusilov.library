9f16988c00cc5e80718423a117fbb5d71d64666c
Vladislav Zakusilov <vvzakusilov@bk.ru>
2023-10-29 22:06:09 +0300

    The class configuration has been changed for Hibernate operation.
    Some new annotations have been added.
    Some old annotations have been deleted.
    The required constructor has been added.

--- src/main/java/ru/zakusilov/library/entity/Book.java
+++ src/main/java/ru/zakusilov/library/entity/Book.java
@@ -1,30 +1,44 @@
 package ru.zakusilov.library.entity;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
+@Entity
+@Table
 @Getter
 @Setter
-@AllArgsConstructor
-@RequiredArgsConstructor
+@NoArgsConstructor
 public class Book {
 
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column
     @NotEmpty(message = "Название книги не может быть пустым")
     @Pattern(regexp = "^[А-ЯA-Z][а-яa-zА-ЯA-Z\\s\\S]*$",
             message = "Название книги должно начинаться с заглавной буквы")
     private String title;
 
+    @Column
     @NotEmpty(message = "Ф.И.О автора не может быть пустым")
     @Size(min = 1, max = 100, message = "Ф.И.О автора должно содержать от 1 до 100 символов")
     @Pattern(regexp = "^[А-ЯЁA-Z][а-яёa-z-]+ [А-ЯЁA-Z][а-яёa-z-]+( [А-ЯЁA-Z][а-яёa-z-]+)?$",
             message = "Полное имя автора должно быть в следующем формате: Фамилия Имя Отчество (при наличии)")
     private String author;
 
+    @Column
     @Min(value = 1500, message = "Год публикации должен быть больше или равен 1500 году")
     private int yearOfPublication;
+
+    public Book(String title, String author, int yearOfPublication) {
+        this.title = title;
+        this.author = author;
+        this.yearOfPublication = yearOfPublication;
+    }
 }
--- src/main/java/ru/zakusilov/library/entity/Person.java
+++ src/main/java/ru/zakusilov/library/entity/Person.java
@@ -1,24 +1,35 @@
 package ru.zakusilov.library.entity;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.*;
 
+@Entity
+@Table
 @Getter
 @Setter
-@AllArgsConstructor
-@RequiredArgsConstructor
-@ToString
+@NoArgsConstructor
 public class Person {
 
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @Column
     @NotEmpty(message = "Полное имя пользователя не может быть пустым")
     @Size(min = 1, max = 100, message = "Полное имя пользователя должно содержать от 1 до 100 символов")
     @Pattern (regexp = "^[А-ЯЁA-Z][а-яёa-z-]+ [А-ЯЁA-Z][а-яёa-z-]+( [А-ЯЁA-Z][а-яёa-z-]+)?$",
             message = "Полное имя пользователя должно быть в следующем формате: Фамилия Имя Отчество (при наличии)")
     private String fullName;
 
+    @Column
     @Min(value = 1900, message = "Год рождения пользователя должен быть больше или равен 1900 году")
     @Max(value = 2010, message = "Год рождения пользователя должен быть меньше или равен 2010 году")
     private int yearOfBirth;
+
+    public Person(String fullName, int yearOfBirth) {
+        this.fullName = fullName;
+        this.yearOfBirth = yearOfBirth;
+    }
 }
