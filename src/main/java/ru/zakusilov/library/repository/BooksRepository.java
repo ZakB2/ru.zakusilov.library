bcd0fac65e9fe790fd4f47aaf507d1d1b4641b3c
Vladislav Zakusilov <vvzakusilov@bk.ru>
2023-10-31 22:46:10 +0300

    Special methods for BookValidator have been added.

--- src/main/java/ru/zakusilov/library/repository/BooksRepository.java
+++ src/main/java/ru/zakusilov/library/repository/BooksRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.zakusilov.library.entity.Book;
 
+import java.util.Optional;
+
 @Repository
 public interface BooksRepository extends JpaRepository<Book, Integer> {
+
+    Optional<Book> findByTitle(String title);
+
+    Optional<Book> findByAuthor(String author);
 }
