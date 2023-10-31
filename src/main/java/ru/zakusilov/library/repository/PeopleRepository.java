e53dd5adacf06b2226782ebebd2ceb3a9180aa81
Vladislav Zakusilov <vvzakusilov@bk.ru>
2023-10-31 22:46:47 +0300

    Special methods for PersonValidator have been added.

--- src/main/java/ru/zakusilov/library/repository/PeopleRepository.java
+++ src/main/java/ru/zakusilov/library/repository/PeopleRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.zakusilov.library.entity.Person;
 
+import java.util.Optional;
+
 @Repository
 public interface PeopleRepository extends JpaRepository<Person, Integer> {
+
+    Optional<Person> findByFullName(String fullName);
+
+    Optional<Person> findByYearOfBirth(int yearOfBirth);
 }
