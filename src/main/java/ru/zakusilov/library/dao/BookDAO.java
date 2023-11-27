822618fd917c48ef9bed385e140f6625d25b79d8
Vladislav Zakusilov <vvzakusilov@bk.ru>
2023-11-27 20:14:18 +0300

    All DAO methods have been deleted.

--- src/main/java/ru/zakusilov/library/dao/BookDAO.java
+++ src/main/java/ru/zakusilov/library/dao/BookDAO.java
@@ -1,41 +1,7 @@
 package ru.zakusilov.library.dao;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-import ru.zakusilov.library.entity.Person;
-
-import java.util.Optional;
 
 @Component
 public class BookDAO {
-
-    private final JdbcTemplate jdbcTemplate;
-
-    @Autowired
-    public BookDAO(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public Optional<Person> findPersonByBookId(int id) {
-        return jdbcTemplate.query("SELECT * " +
-                                "FROM Person " +
-                                "JOIN Book " +
-                                "ON Person.id = Book.person_id " +
-                                "WHERE Book.id = ?",
-                        new Object[]{id},
-                        new BeanPropertyRowMapper<>(Person.class)).
-                stream().findAny();
-    }
-
-    public void selectBook(int id, Person selectedPerson) {
-        jdbcTemplate.update("UPDATE Book SET person_id = ? WHERE id = ?",
-                selectedPerson.getId(),
-                id);
-    }
-
-    public void returnBook(int id) {
-        jdbcTemplate.update("UPDATE Book SET person_id = NULL WHERE id = ?", id);
-    }
 }
