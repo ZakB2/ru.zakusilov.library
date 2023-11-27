d1df68d583293a7a5bc1eb31803010b003a76a22
Vladislav Zakusilov <vvzakusilov@bk.ru>
2023-11-27 20:14:33 +0300

    All DAO methods have been deleted.

--- src/main/java/ru/zakusilov/library/dao/PersonDAO.java
+++ src/main/java/ru/zakusilov/library/dao/PersonDAO.java
@@ -1,28 +1,7 @@
 package ru.zakusilov.library.dao;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
-import ru.zakusilov.library.entity.Book;
-
-import java.util.List;
 
 @Component
 public class PersonDAO {
-
-    private final JdbcTemplate jdbcTemplate;
-
-    @Autowired
-    public PersonDAO(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public List<Book> findBooksByPersonId(int id) {
-        return jdbcTemplate.query("SELECT * " +
-                        "FROM Book " +
-                        "WHERE person_id = ?",
-                new Object[]{id},
-                new BeanPropertyRowMapper<>(Book.class));
-    }
 }
