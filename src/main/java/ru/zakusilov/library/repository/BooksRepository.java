d16793ca8e17403b39e2554b89c1fb7a1ffd529a
Vladislav Zakusilov <vvzakusilov@bk.ru>
2023-11-28 22:02:17 +0300

    The new method has been added.
    The findByTitleStartingWith() method has been added to search for a book.

--- src/main/java/ru/zakusilov/library/repository/BooksRepository.java
+++ src/main/java/ru/zakusilov/library/repository/BooksRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.zakusilov.library.entity.Book;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,4 +13,6 @@ public interface BooksRepository extends JpaRepository<Book, Integer> {
     Optional<Book> findByTitle(String title);
 
     Optional<Book> findByAuthor(String author);
+
+    List<Book> findByTitleStartingWith(String title);
 }
