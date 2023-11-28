67f90d9bb3011df53b4a22f893b063f07636a6de
Vladislav Zakusilov <vvzakusilov@bk.ru>
2023-11-28 22:22:57 +0300

    The new method has been added.
    The findAllOrderById() method has been added to display the person by "id" order.

--- src/main/java/ru/zakusilov/library/repository/PeopleRepository.java
+++ src/main/java/ru/zakusilov/library/repository/PeopleRepository.java
@@ -1,13 +1,18 @@
 package ru.zakusilov.library.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.zakusilov.library.entity.Person;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PeopleRepository extends JpaRepository<Person, Integer> {
+
+    @Query("SELECT p FROM Person p ORDER BY p.id")
+    List<Person> findAllOrderById();
 
     Optional<Person> findByFullName(String fullName);
 
